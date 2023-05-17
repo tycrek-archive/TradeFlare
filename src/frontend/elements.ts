0b17159e5065195aa0bb9276643b982baf5df5d1
tycrek <gpg+github@jmoore.dev>
2023-05-17 00:24:21 -0600

    fix: made element names more consistent

--- src/frontend/elements.ts
+++ src/frontend/elements.ts
@@ -4,19 +4,19 @@ export const Elements = {
 	},
 	NewUser: { // * New user flow
 		newButton: document.querySelector('#NewUserButton') as HTMLElement,
-		newDialog: document.querySelector('#new-modal') as HTMLElement,
+		newDialog: document.querySelector('#NewUserModal') as HTMLElement,
 
-		registerButton: document.querySelector('sl-button#register') as HTMLElement,
-		callSignInput: document.querySelector('sl-input#call-sign') as HTMLInputElement,
-		factionInput: document.querySelector('sl-input#faction') as HTMLInputElement,
+		callSignInput: document.querySelector('#NewUserCallSign') as HTMLInputElement,
+		factionInput: document.querySelector('#NewUserFaction') as HTMLInputElement,
+		registerButton: document.querySelector('#NewUserRegister') as HTMLElement,
 
-		successDialog: document.querySelector('#success-dialog') as HTMLElement,
-		successName: document.querySelector('#success-name') as HTMLElement,
-		copyNewTokenButton: document.querySelector('#CopyNewTokenButton') as HTMLElement,
+		successDialog: document.querySelector('#NewUserSuccessDialog') as HTMLElement,
+		successName: document.querySelector('#NUSDName') as HTMLElement,
+		copyNewTokenButton: document.querySelector('#NUSDCopyNewTokenButton') as HTMLElement,
 	},
 	AgentPanel: { // * Agent panel
-		name: document.querySelector('#agent-name') as HTMLElement,
-		accountId: document.querySelector('#account-id') as HTMLElement,
-		location: document.querySelector('#agent-location') as HTMLElement,
+		name: document.querySelector('#AgentName') as HTMLElement,
+		accountId: document.querySelector('#AgentAccountId') as HTMLElement,
+		location: document.querySelector('#AgentLocation') as HTMLElement,
 	}
 }
